20236a459bcbd4169ecceddf4f75590df5f2decd
Han Lai <x94lexie@gmail.com>
2022-07-08 22:53:57 +0800

    style: 修改 HomePage - cards container max-width and padding

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,45 +1,48 @@
 <template>
-  <q-page class="constrain row justify-center q-pa-md">
-    <div class="col">
-      <ul class="list-unstyled">
-        <li v-if="posts.length === 0" class="text-center">
-          <SkeletonCard v-if="loading" />
-          <q-card v-else flat bordered>
-            <q-card-section class="text-center">
-              發布第一則貼文吧！
-            </q-card-section>
-          </q-card>
-        </li>
-        <li v-for="post in posts" :key="post._id">
-          <PostCard :data="post" />
-        </li>
-      </ul>
-    </div>
+  <q-page>
+    <div class="constrain row justify-center q-py-md">
+      <div class="col-auto">
+        <ul class="list-unstyled constrain-cards">
+          <li v-if="posts.length === 0" class="text-center">
+            <SkeletonCard v-if="loading" />
+            <q-card v-else flat bordered>
+              <q-card-section class="text-center">
+                發布第一則貼文吧！
+              </q-card-section>
+            </q-card>
+          </li>
 
-    <div class="col-4 lg-screen-only">
-      <q-item class="fixed">
-        <q-item-section avatar>
-          <BaseAvatar :src="authStore.user.avatar" />
-        </q-item-section>
+          <li v-for="post in posts" :key="post._id">
+            <PostCard :data="post" />
+          </li>
+        </ul>
+      </div>
 
-        <q-item-section>
-          <q-item-label class="text-bold">{{
-            authStore.user.screenName
-          }}</q-item-label>
-        </q-item-section>
-      </q-item>
+      <div class="col-4 lg-screen-only">
+        <q-item class="fixed">
+          <q-item-section avatar>
+            <BaseAvatar :src="authStore.user.avatar" />
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label class="text-bold">{{
+              authStore.user.screenName
+            }}</q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
     </div>
   </q-page>
 </template>
 
 <script setup>
 import { ref } from "vue";
-import BaseAvatar from "src/components/BaseAvatar.vue";
-import SkeletonCard from "src/components/SkeletonCard.vue";
-import PostCard from "src/components/PostCard.vue";
 import { useAuthStore } from "src/stores/authStore";
 import { apiGetPosts } from "src/apis";
 import notifyApiError from "src/utility/notifyApiError";
+import BaseAvatar from "src/components/BaseAvatar.vue";
+import SkeletonCard from "src/components/SkeletonCard.vue";
+import PostCard from "src/components/PostCard.vue";
 
 const authStore = useAuthStore();
 
@@ -61,4 +64,8 @@ const getData = async () => {
 getData();
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.constrain-cards {
+  max-width: 470px;
+}
+</style>
