e821811384b06fb44c9a962e1a9632d2f7ae3048
Han Lai <x94lexie@gmail.com>
2022-07-16 00:59:35 +0800

    fix: GET api 無法傳 params
    原因: axios 語法寫錯

--- src/apis/index.js
+++ src/apis/index.js
@@ -3,10 +3,12 @@ import { api } from "boot/axios";
 const USER_URL = "users";
 export const apiLogin = (data) => api.post(`${USER_URL}/login`, data);
 export const apiSignup = (data) => api.post(`${USER_URL}/signup`, data);
+export const apiGetProfile = (id) => api.post(`${USER_URL}/${id}/profile`);
 
 const POST_URL = "posts";
-export const apiGetPosts = (data) => api.get(`${POST_URL}`, data);
-export const apiGetPost = (id, data) => api.get(`${POST_URL}/${id}`, data);
+export const apiGetPosts = (params) => api.get(`${POST_URL}`, { params });
+export const apiGetPost = (id, params) =>
+  api.get(`${POST_URL}/${id}`, { params });
 export const apiAddComment = (id, data) =>
   api.post(`${POST_URL}/${id}/comment`, data);
 export const apiDeleteComment = (id) => api.delete(`${POST_URL}/comment/${id}`);
