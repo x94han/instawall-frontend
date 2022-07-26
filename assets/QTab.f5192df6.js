b0795e9e7dca13e3080929fef0e0590547fed6b6
Han Lai <x94lexie@gmail.com>
2022-07-27 02:29:13 +0800

    3cc39a5

---
+++ assets/QTab.f5192df6.js
@@ -0,0 +1,1 @@
+import{u as o,a as t,b as m}from"./QTabs.c7351fdd.js";import{c as p}from"./index.ef0b161e.js";var u=p({name:"QTab",props:o,emits:t,setup(a,{slots:s,emit:e}){const{renderTab:r}=m(a,s,e);return()=>r("div")}});export{u as Q};
